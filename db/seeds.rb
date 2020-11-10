77e866d5f6b238995574251e737a452b3e5f19b7
timthomasdev <tim.thomas@hey.com>
2020-11-10 11:42:32 -0600

    Create warehouse index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ware.create!(name: 'Extreme Programming Explained', description: "Second Edition of Extreme Programming Explained by Kent Beck with Cynthia Andres, Lightly used", price: "15.00")
+
+Ware.create!(name: 'Apple Watch Series 3', description: "Black aluminum 37mm with basic wristband. Purchased in 2018", price: "75.00")
+
+Ware.create!(name: 'Nothing', description: "You will receive nothing after purchasing this item, I think $10 is a fair price for nothing", price: "10.00")
