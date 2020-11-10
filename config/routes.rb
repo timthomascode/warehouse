0e9c5fee8c487849db3fc6baa0962f9f69703aa5
timthomasdev <tim.thomas@hey.com>
2020-11-10 13:07:08 -0600

    Add /login and /logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   get 'warehouse/index'
   resources :wares
   devise_for :admins
+  devise_scope :admin do
+    get 'login', to: 'devise/sessions#new'
+    get 'logout', to: 'devise/sessions#destroy'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
