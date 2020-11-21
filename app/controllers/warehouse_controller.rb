54c48c8353a8475c7a3e1104ae8aa40cc799b2b2
timthomasdev <tim.thomas@hey.com>
2020-11-20 21:27:09 -0600

    Reset ware status if order canceled before checkout

--- app/controllers/warehouse_controller.rb
+++ app/controllers/warehouse_controller.rb
@@ -1,5 +1,10 @@
 class WarehouseController < ApplicationController
   def index
+    if session[:processed_ware]
+      ware = Ware.find(session[:processed_ware])
+      ware.update!(status: :available) unless ware.status == :sold
+      session[:processed_ware] = nil
+    end
     @wares = Ware.all
   end
 end
