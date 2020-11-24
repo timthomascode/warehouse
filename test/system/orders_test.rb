9544ac23c67ca3a9509d047215044a606344851d
timthomasdev <tim.thomas@hey.com>
2020-11-24 17:26:31 -0600

    Add end-to-end test for successful order payment

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -13,21 +13,34 @@ class OrdersTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Orders"
   end
 
-  test "creating an Order" do
+  test "successful order & payment flow" do
     visit warehouse_index_url
     click_on "Buy now", match: :first
 
-    fill_in "Apt num", with: @order.apt_num
-    fill_in "City", with: @order.city
-    fill_in "Email", with: @order.email
-    fill_in "First name", with: @order.first_name
-    fill_in "Last name", with: @order.last_name
-    fill_in "State", with: @order.state
-    fill_in "Street address", with: @order.street_address
-    fill_in "Zip code", with: @order.zip_code
-    click_on "Checkout"
+    assert_text "New Order"
 
-    assert_text "Order was successfully created"
+    fill_in "First name", with: "Tester"
+    fill_in "Last name", with: "McTest"
+    fill_in "Street address", with: "123 System Test Lane"
+    fill_in "City", with: "Capybara"
+    fill_in "State", with: "System Test"
+    fill_in "Zip code", with: "12345"
+    fill_in "Email", with: "test@example.com"
+    click_on "Confirm Order"
+
+    assert_text "Review Order"
+    click_on "Pay"
+
+    assert_text "test@example.com"
+    fill_in "cardNumber", with: "4242424242424242"
+    fill_in "cardExpiry", with: "424"
+    fill_in "cardCvc", with: "424"
+    fill_in "billingName", with: "Tester McTest"
+    fill_in "billingPostalCode", with: "12345"
+    click_on "Pay"
+
+    assert_text "paid: true", wait: 5
+    
   end
 
   test "destroying an Order" do
