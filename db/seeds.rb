8bbc34988a591af40f65d6aba4ccbd0f13eb71b6
timthomasdev <tim.thomas@hey.com>
2020-12-01 14:18:36 -0600

    Update ware seeds with test image

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Ware.create!(name: 'Extreme Programming Explained', description: "Second Edition of Extreme Programming Explained by Kent Beck with Cynthia Andres, Lightly used", price_cents: 1500) 
+ware = Ware.new(name: 'Extreme Programming Explained', description: "Second Edition of Extreme Programming Explained by Kent Beck with Cynthia Andres, Lightly used", price_cents: 1500, status: :available) 
+ware.image.attach(io: File.open('app/assets/images/test.png'), filename: 'test.png', content_type: 'image/png')
+ware.save!
 
-Ware.create!(name: 'Apple Watch Series 3', description: "Black aluminum 37mm with basic wristband. Purchased in 2018", price_cents: 7500)
+ware = Ware.new(name: 'Apple Watch Series 3', description: "Black aluminum 37mm with basic wristband. Purchased in 2018", price_cents: 7500, status: :available)
+ware.image.attach(io: File.open('app/assets/images/test.png'), filename: 'test.png', content_type: 'image/png')
+ware.save!
 
-Ware.create!(name: 'Nothing', description: "You will receive nothing after purchasing this item, I think $10 is a fair price for nothing", price_cents: 1000)
+ware = Ware.new(name: 'Nothing', description: "You will receive nothing after purchasing this item, I think $10 is a fair price for nothing", price_cents: 1000, status: :available)
+ware.image.attach(io: File.open('app/assets/images/test.png'), filename: 'test.png', content_type: 'image/png')
+ware.save!
+
+
