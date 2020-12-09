68194e5cc3caa6e56770c25ef34a65998dd6cde7
timthomasdev <tim.thomas@hey.com>
2020-12-08 18:27:35 -0600

    Add test for ware.price

--- test/models/ware_test.rb
+++ test/models/ware_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class WareTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "price returns US dollars" do
+    ware = Ware.new(name: "Test Ware", description: "Test description for the Test Ware", price_cents: 1155)
+    assert_equal "$11.55", ware.price
+  end
 end
