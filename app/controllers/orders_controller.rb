a4b7f4089fb38277280b1075ff8f756a30d1bb1a
timthomasdev <tim.thomas@hey.com>
2020-12-09 23:26:24 -0600

    Add checkout session ID to the success_url

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -88,7 +88,7 @@ class OrdersController < ApplicationController
           quantity: 1,
         }],
         mode: 'payment',
-        success_url: success_url,
+        success_url: "#{ success_url }?session_id={CHECKOUT_SESSION_ID}",
         cancel_url: cancel_url,
       })
 
