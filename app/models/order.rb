765b4d20a87d1f40d841ab0b67c13ce9675ce755
timthomasdev <tim.thomas@hey.com>
2020-12-10 00:05:31 -0600

    Remove Order#complete

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,13 +2,6 @@ class Order < ApplicationRecord
   belongs_to :ware
   validates :first_name, :last_name, :street_address, :city, :state, :zip_code, :email, :ware_id, :checkout_session, presence: true 
 
-  def self.complete(order_id)
-    order = Order.find(order_id)
-    order.mark_paid
-    order.ware.mark_sold
-    order
-  end
-
   def complete
     self.mark_paid
     self.ware.mark_sold
