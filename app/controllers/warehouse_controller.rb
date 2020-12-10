65c52d87619077bbbda045fae64b3d322504e1cf
timthomasdev <tim.thomas@hey.com>
2020-12-10 00:38:56 -0600

    Prevent sold ware from being redisplayed in warehouse index

--- app/controllers/warehouse_controller.rb
+++ app/controllers/warehouse_controller.rb
@@ -2,7 +2,9 @@ class WarehouseController < ApplicationController
   def index
     if session[:processed_ware]
       ware = Ware.find(session[:processed_ware])
-      ware.update!(status: :available) unless ware.status == :sold
+      if ware.processing? 
+        ware.update!(status: :available)
+      end
       @wares = Ware.where(status: :available).order(:price_cents)
       ActionCable.server.broadcast('warehouse', { html: render_to_string('warehouse/index', layout: false )})
       session[:processed_ware] = nil
