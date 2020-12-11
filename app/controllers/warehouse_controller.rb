ae0c39c127f605b6c54cb19d66435290b21bff7b
timthomasdev <tim.thomas@hey.com>
2020-12-10 20:29:06 -0600

    Pull session deletion out of conditional in Warehouse#index

--- app/controllers/warehouse_controller.rb
+++ app/controllers/warehouse_controller.rb
@@ -6,8 +6,8 @@ class WarehouseController < ApplicationController
         ware.update!(status: :available)
         broadcast_wares_to_warehouse
       end
-      session[:processed_ware] = nil
     end
+    session.delete(:processed_ware)
     @wares = Ware.where(status: :available).order(:price_cents)
   end
 end
